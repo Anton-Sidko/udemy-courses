074896ec68d25490f75a7f682cbadc1c13b5a88c
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2022-12-02 09:48:15 +0200

    Added navbar

---
+++ complete-react-developer/e-store-clothing/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,34 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+  return (
+    // Fragment or <></>
+    <Fragment>
+      <div className="navigation">
+        <Link
+          className="logo-container"
+          to="/"
+        >
+          <CrwnLogo className="logo" />
+        </Link>
+
+        <nav className="nav-links-container">
+          <Link
+            className="nav-link"
+            to="/shop"
+          >
+            SHOP
+          </Link>
+        </nav>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
