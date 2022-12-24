800a4732da834fbaa924248a80950165b67b7f58
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2022-12-24 11:39:41 +0200

    Added cart items to cart dropdown

--- complete-react-developer/e-store-clothing/src/components/product-card/product-card.component.jsx
+++ complete-react-developer/e-store-clothing/src/components/product-card/product-card.component.jsx
@@ -1,9 +1,16 @@
+import { useContext } from 'react';
+
 import Button from '../button/button.component';
+
+import { CartContext } from '../../context/cart.context';
 
 import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
 
   return (
     <div className="product-card-container">
@@ -17,7 +24,12 @@ const ProductCard = ({ product }) => {
         <span className="price">{price}</span>
       </div>
 
-      <Button buttonType="inverted">Add to cart</Button>
+      <Button
+        buttonType="inverted"
+        onClick={addProductToCart}
+      >
+        Add to cart
+      </Button>
     </div>
   );
 };
