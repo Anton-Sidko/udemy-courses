284eb3008c46275ecfa11a8ce3911cafc1c3ca20
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2023-01-02 09:30:27 +0200

    Added Redux-Persist

--- complete-react-developer/e-store-clothing/src/store/store.js
+++ complete-react-developer/e-store-clothing/src/store/store.js
@@ -1,4 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import logger from 'redux-logger'; //INFO not necessary but help see how store works
 
 import { rootReducer } from './root-reducer';
@@ -22,6 +25,21 @@ import { rootReducer } from './root-reducer';
 // const middleWares = [loggerMiddleware];
 const middleWares = [logger];
 
+// INFO Redux persist config
+const persistConfig = {
+  key: 'root',
+  storage,
+  blackList: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
