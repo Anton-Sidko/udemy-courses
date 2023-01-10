8a0688ff5b0ec31fc0a19f4fb72b1716d094b1d5
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2023-01-10 13:55:18 +0200

    Typed Sagas

--- complete-react-developer/e-store-clothing/src/store/user/user.action.ts
+++ complete-react-developer/e-store-clothing/src/store/user/user.action.ts
@@ -7,6 +7,8 @@ import {
   ActionWithPayload,
 } from '../../utils/reducer/reducer.utils';
 
+import { User } from 'firebase/auth';
+
 import {
   UserData,
   AdditionalInformation,
@@ -44,7 +46,7 @@ export type SignUpStart = ActionWithPayload<
 
 export type SignUpSuccess = ActionWithPayload<
   USER_ACTION_TYPES.SIGN_UP_SUCCESS,
-  { user: UserData; additionalDetails: AdditionalInformation }
+  { user: User; additionalDetails: AdditionalInformation }
 >;
 
 export type SignUpFailed = ActionWithPayload<
@@ -81,7 +83,7 @@ export const emailSignInStart = withMatcher(
 );
 
 export const signInSuccess = withMatcher(
-  (user: UserData): SignInSuccess =>
+  (user: UserData & { id: string }): SignInSuccess =>
     createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user)
 );
 
@@ -100,7 +102,7 @@ export const signUpStart = withMatcher(
 );
 
 export const signUpSuccess = withMatcher(
-  (user: UserData, additionalDetails: AdditionalInformation): SignUpSuccess =>
+  (user: User, additionalDetails: AdditionalInformation): SignUpSuccess =>
     createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalDetails })
 );
 
