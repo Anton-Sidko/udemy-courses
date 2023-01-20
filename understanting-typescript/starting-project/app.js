3f57249c62f99cab39037b351c7d490697c8d326
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2023-01-20 09:17:21 +0200

    Added referense project

--- understanting-typescript/starting-project/app.js
+++ understanting-typescript/starting-project/app.js
@@ -1,5 +1,25 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //INFO tuple
+// } = {
+//   // const person = {
+//   name: 'Anton',
+//   age: 32,
+//   hobbies: ['read', 'computer games'],
+//   role: [2, 'developer'], //INFO tuple
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["DEVELOPER"] = 2] = "DEVELOPER";
+})(Role || (Role = {}));
 var person = {
     name: 'Anton',
-    age: 32
+    age: 32,
+    hobbies: ['read', 'computer games'],
+    role: Role.DEVELOPER
 };
 console.log(person);
