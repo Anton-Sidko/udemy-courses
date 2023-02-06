b88d3409f32f8ba90b60b628a4bb4f1724ff2dcd
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2023-02-06 09:37:15 +0200

    Organized files with Namespaces

---
+++ understanting-typescript/03-practice-project/src/components/project-input.ts
@@ -0,0 +1,93 @@
+/// <reference path="../util/validation.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="base-component.ts" />
+/// <reference path="../state/project-state.ts" />
+
+namespace App {
+  // ProjectInput class
+  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+
+    constructor() {
+      super('project-input', 'app', true, 'user-input');
+
+      this.titleInputElement = this.element.querySelector(
+        '#title'
+      ) as HTMLInputElement;
+      this.descriptionInputElement = this.element.querySelector(
+        '#description'
+      ) as HTMLInputElement;
+      this.peopleInputElement = this.element.querySelector(
+        '#people'
+      ) as HTMLInputElement;
+
+      this.configure();
+    }
+
+    configure() {
+      this.element.addEventListener('submit', this.submitHandler);
+    }
+
+    renderContent(): void {}
+
+    @Autobind
+    private submitHandler(event: Event) {
+      event.preventDefault();
+      const userInput = this.gatherUserInput();
+
+      if (Array.isArray(userInput)) {
+        const [title, description, people] = userInput;
+
+        projectState.addProject(title, description, people);
+
+        this.clearUserInputs();
+      }
+    }
+
+    private gatherUserInput(): [string, string, number] | undefined {
+      const enteredTitle = this.titleInputElement.value;
+      const enteredDescription = this.descriptionInputElement.value;
+      const enteredPeople = this.peopleInputElement.value;
+
+      const titleValidatable: Validatable = {
+        value: enteredTitle,
+        required: true,
+        minLength: 5,
+        maxLength: 25,
+      };
+
+      const descriptionValidatable: Validatable = {
+        value: enteredDescription,
+        required: true,
+        minLength: 10,
+        maxLength: 250,
+      };
+
+      const peopleValidatable: Validatable = {
+        value: enteredPeople,
+        required: true,
+        min: 1,
+        max: 15,
+      };
+
+      if (
+        validate(titleValidatable) &&
+        validate(descriptionValidatable) &&
+        validate(peopleValidatable)
+      ) {
+        return [enteredTitle, enteredDescription, +enteredPeople];
+      } else {
+        alert('⚠Invalid input, please try again!⚠');
+        return;
+      }
+    }
+
+    private clearUserInputs() {
+      this.titleInputElement.value = '';
+      this.descriptionInputElement.value = '';
+      this.peopleInputElement.value = '';
+    }
+  }
+}
---
+++ understanting-typescript/03-practice-project/src/decorators/autobind.ts
@@ -0,0 +1,19 @@
+namespace App {
+  // INFO Auto-bind decorator
+  export const Autobind = function (
+    _: any,
+    _2: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjustedDescriptor: PropertyDescriptor = {
+      configurable: true,
+      enumerable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjustedDescriptor;
+  };
+}
