8a20b530b0533fb1ca42c8e48c1bcea13b540f6f
Anton Sidko <sidko.antonanatoljevich@gmail.com>
2023-07-18 11:44:36 +0300

    Added workout timer project

--- the-ultimate-react-course2023/13-workout-timer/src/Calculator.js
+++ the-ultimate-react-course2023/13-workout-timer/src/Calculator.js
@@ -1,5 +1,5 @@
-import { memo, useEffect, useState } from "react";
-import clickSound from "./ClickSound.m4a";
+import { memo, useEffect, useState } from 'react';
+import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
   const [number, setNumber] = useState(workouts.at(0).numExercises);
@@ -9,62 +9,44 @@ function Calculator({ workouts, allowSound }) {
 
   const [duration, setDuration] = useState(0);
 
-  // const playSound = useCallback(
-  //   function () {
-  //     if (!allowSound) return;
-  //     const sound = new Audio(clickSound);
-  //     sound.play();
-  //   },
-  //   [allowSound]
-  // );
+  useEffect(() => {
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
+  }, [number, sets, speed, durationBreak]);
 
-  useEffect(
-    function () {
-      setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
-    },
-    [number, sets, speed, durationBreak]
-  );
+  useEffect(() => {
+    const playSound = function () {
+      if (!allowSound) return;
+      const sound = new Audio(clickSound);
+      sound.play();
+    };
 
-  useEffect(
-    function () {
-      const playSound = function () {
-        if (!allowSound) return;
-        const sound = new Audio(clickSound);
-        sound.play();
-      };
-
-      playSound();
-    },
-    [duration, allowSound]
-  );
-
-  useEffect(
-    function () {
-      console.log(duration, sets);
-      document.title = `Your ${number}-exercise workout`;
-    },
-    [number, duration, sets]
-  );
+    playSound();
+  }, [duration, allowSound]);
 
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  function handleInc() {
-    setDuration((duration) => Math.floor(duration) + 1);
-  }
-
-  function handleDec() {
-    setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
-  }
+  const handleIncDuration = function () {
+    setDuration((duration) => Math.floor(duration + 1));
+  };
+  const handleDecDuration = function () {
+    setDuration((duration) => (duration > 1 ? Math.ceil(duration - 1) : 0));
+  };
 
   return (
     <>
       <form>
         <div>
           <label>Type of workout</label>
-          <select value={number} onChange={(e) => setNumber(+e.target.value)}>
+          <select
+            value={number}
+            onChange={(e) => setNumber(+e.target.value)}
+          >
             {workouts.map((workout) => (
-              <option value={workout.numExercises} key={workout.name}>
+              <option
+                value={workout.numExercises}
+                key={workout.name}
+              >
                 {workout.name} ({workout.numExercises} exercises)
               </option>
             ))}
@@ -106,16 +88,17 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={handleDec}>–</button>
+        <button onClick={handleDecDuration}>–</button>
         <p>
-          {mins < 10 && "0"}
-          {mins}:{seconds < 10 && "0"}
+          {mins < 10 && '0'}
+          {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={handleInc}>+</button>
+        <button onClick={handleIncDuration}>+</button>
       </section>
     </>
   );
 }
 
 export default memo(Calculator);
+
